324a50fb944ccfa79b62ed1d47ec00a2e24eadbe
Gautam Bajaj <gautam.bajaj.ug23@nsut.ac.in>
2026-05-27 12:49:25 +0530

    fix deployment issue

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://fullstack-chat-app-ev4k.onrender.com" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "https://fullstack-chat-app-ev4k.onrender.com/api",
   withCredentials: true,
 });
