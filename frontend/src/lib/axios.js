12ae641e4ed567428f5d384107f60671d728820c
Gautam Bajaj <gautam.bajaj.ug23@nsut.ac.in>
2026-05-27 12:27:41 +0530

    Updates latest

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://fullstack-chat-app-ev4k.onrender.com" : "/api",
   withCredentials: true,
 });
